ef6ba6b648cd318d44640526a4c599ed04cf9f6e
musangking15 <haikalfajari156@gmail.com>
2024-10-07 16:27:32 +0700

    Fix the display on the transaction page to show the receipt

--- app/Views/admin/transaksi.php
+++ app/Views/admin/transaksi.php
@@ -10,9 +10,18 @@
         <div class="col-sm-4 mb-3 mb-sm-0">
             <div class="card mb-3">
                 <div class="card-header bg-primary text-light shadow">
-                    <h5 class="card-title">
-                        Customer: <?= $value['nama_customer']; ?>
-                    </h5>
+                    <div class="row">
+                        <div class="col-8">
+                            <h5 class="card-title">
+                                Customer: <?= $value['nama_customer']; ?>
+                            </h5>
+                        </div>
+                        <div class="col-4">
+                            <button class="btn btn-warning btn-sm" data-toggle="modal" data-target="#exampleModal-<?= $value['order_id']; ?>">
+                                Bukti Bayar
+                            </button>
+                        </div>
+                    </div>
                 </div>
                 <div class="card-body">
                     <?php foreach ($value['pesanan'] as $item) : ?>
@@ -31,13 +40,30 @@
                         <?= csrf_field(); ?>
                         <button class="btn btn-sm btn-success" type="submit">Ready</i></button>
                     </form>
-                    <form action="" method="post" class="d-inline">
-                        <?= csrf_field(); ?>
-                        <button class="btn btn-sm btn-warning" type="submit">paid</i></button>
-                    </form>
+                </div>
+            </div>
+        </div>
+    <?php endforeach ?>
+
+    <!-- Modal bukti bayar -->
+    <?php foreach ($transaksi as $value) : ?>
+        <div class="modal fade" id="exampleModal-<?= $value['order_id']; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="buktiModalLabel-<?= $value['id_transaksi']; ?>">Bukti Pembayaran</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body text-center">
+                        <img src="<?= base_url('bukti/' . $value['receipt']); ?>" alt="Bukti Pembayaran" class="img-fluid">
+                    </div>
                 </div>
             </div>
         </div>
     <?php endforeach ?>
 </div>
+
+
 <?= $this->endSection() ?>
